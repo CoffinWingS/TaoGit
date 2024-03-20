3f23e80412e4a41986dc0f52d7056c97ccce19b7
CoffinWingS <156872580+CoffinWingS@users.noreply.github.com>
2024-03-20 22:07:59 +0700

    Update FuriTao.py

--- FuriTao.py
+++ FuriTao.py
@@ -9,6 +9,16 @@ st.write(df.head(10))
 
 st.subheader("อัตราการฆ่าตัวตายในแต่ละประเทศ")
 
+import numpy as np
+
+df = pd.read_csv('/content/master.csv')
+
+# แปลงคอลัมน์ "country" เป็น float
+df["country"] = pd.to_numeric(df["country"], errors="coerce").astype(np.float64)
+
+# บันทึกข้อมูลกลับไปยังไฟล์ CSV
+df.to_csv('/content/master.csv', index=False)
+
 # นับจำนวนผู้ฆ่าตัวตายในแต่ละประเทศ
 counts = df.groupby("country").size()
 
