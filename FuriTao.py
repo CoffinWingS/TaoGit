a79be5a21c1a9044966e2ca0bfa57f1d7f187f52
CoffinWingS <156872580+CoffinWingS@users.noreply.github.com>
2024-03-20 22:12:04 +0700

    Update FuriTao.py

--- FuriTao.py
+++ FuriTao.py
@@ -1,23 +1,13 @@
 import streamlit as st
 import pandas as pd
 
-df=pd.read_csv('./data/master.csv')
+df=pd.read_csv('./data/masterr.csv')
 
 st.header("สถิติการฆ่าตัวต่ายในปี 1987 - 2014")
 
 st.write(df.head(10))
 
 st.subheader("อัตราการฆ่าตัวตายในแต่ละประเทศ")
-
-import numpy as np
-
-df = pd.read_csv('/content/master.csv')
-
-# แปลงคอลัมน์ "country" เป็น float
-df["country"] = pd.to_numeric(df["country"], errors="coerce").astype(np.float64)
-
-# บันทึกข้อมูลกลับไปยังไฟล์ CSV
-df.to_csv('/content/master.csv', index=False)
 
 # นับจำนวนผู้ฆ่าตัวตายในแต่ละประเทศ
 counts = df.groupby("country").size()
