3217265324ccbe91309fa57e28eb139957ccbcd6
CoffinWingS <156872580+CoffinWingS@users.noreply.github.com>
2024-03-20 22:45:33 +0700

    Update FuriTao.py

--- FuriTao.py
+++ FuriTao.py
@@ -19,31 +19,14 @@ df_country = df.groupby('country').size()
 df_country = df_country.sort_values(ascending=False)
 
 # แสดงกราฟแท่งแนวนอน
-#plt.barh(df_country.index, df_country)
-#plt.xlabel('Furina For Like')
-#plt.ylabel('HuTao for Love')
-#plt.show()
-
-
-# ปรับขนาดตัวอักษร
-plt.rcParams['font.size'] = 10
-plt.xlabel('Furina For Like', fontsize=10)
-plt.ylabel('HuTao for Love', fontsize=10)
-plt.xticks(fontsize=8)
-plt.yticks(fontsize=8)
-
-# แสดงกราฟแท่งแนวนอน
 plt.barh(df_country.index, df_country)
-
-# แสดงชื่อประเทศ
-for i in range(len(df_country)):
-    plt.text(df_country[i], i, df_country.index[i], ha='right', fontsize=8)
-
+plt.xlabel('Furina For Like')
+plt.ylabel('HuTao for Love')
 plt.show()
-st.pyplot()
+
 
 # แสดงกราฟบน Streamlit
-#st.pyplot(plt)
+st.pyplot(plt)
 
 
 
