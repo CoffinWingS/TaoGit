3de3345017b2a4bd9deba4984732f04127ef4a8b
CoffinWingS <156872580+CoffinWingS@users.noreply.github.com>
2024-03-21 02:09:28 +0700

    Update FuriTao.py

--- FuriTao.py
+++ FuriTao.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 
-df=pd.read_csv('./data/master.csv')
+df=pd.read_csv('./data/masteerrp.csv')
 
 st.header("สถิติการฆ่าตัวต่ายในปี 1987 - 2014")
 
