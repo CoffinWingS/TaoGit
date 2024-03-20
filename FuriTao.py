ac2c9b0c5d81df79192fb6c9c7b110400541dd1d
CoffinWingS <156872580+CoffinWingS@users.noreply.github.com>
2024-03-21 00:46:16 +0700

    Update FuriTao.py

--- FuriTao.py
+++ FuriTao.py
@@ -46,7 +46,19 @@ else:
     st.button("ปิดไปเลย")
 
 #***************************************************************************************************
-df.groupby('year').count()['suicides_no'].plot.line()
+# เลือกข้อมูลปี 1987 ถึง 2014
+df = df[df['year'] >= 1987]
+df = df[df['year'] <= 2014]
+
+# กรองข้อมูลเฉพาะประเทศไทย
+#df = df[df['country'] == 'Thailand']
+
+# สร้างกราฟเส้น
+plt.plot(df['suicides_no'],df['year'])
+plt.xlabel('Year')
+plt.ylabel('Suicide Rate')
+plt.show()
+st.pyplot()
 
 
 
