7ab2a394f5713181aff4aa717429d0b5de2b0d64
CoffinWingS <156872580+CoffinWingS@users.noreply.github.com>
2024-03-21 02:30:55 +0700

    Update FuriTao.py

--- FuriTao.py
+++ FuriTao.py
@@ -69,7 +69,7 @@ if(st.button("แบบกราฟเส้น")):
  
  df_year = df.groupby('year').size()
 
- plt.style.use('default')
+ plt.style.use('seaborn-v0_8-whitegrid')
 
  plt.title('Trend of Suicide Rates from 1987 to 2014', fontsize=16)
  # แสดงกราฟเส้น
