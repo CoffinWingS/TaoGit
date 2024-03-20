5618f306ec0dd71da3e43a6d04385a59045aed99
CoffinWingS <156872580+CoffinWingS@users.noreply.github.com>
2024-03-21 00:03:05 +0700

    Update FuriTao.py

--- FuriTao.py
+++ FuriTao.py
@@ -28,6 +28,9 @@ for i, v in enumerate(df_country):
 # เพิ่มกริด
 plt.grid(axis='x')
 
+# เพิ่มกรอบ
+plt.box(False)
+
 # แสดงกราฟแท่งแนวนอน
 #plt.barh(df_country.index, df_country, linewidth=3)
 plt.barh(df_country.index, df_country, color='#ff5722')
