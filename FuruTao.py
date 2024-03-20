ccad4839fd07a3e5384270975de4126bf37d38a8
CoffinWingS <156872580+CoffinWingS@users.noreply.github.com>
2024-03-20 12:19:40 +0700

    Update FuruTao.py

--- FuruTao.py
+++ FuruTao.py
@@ -1,1 +1,11 @@
+import streamlit as st
+import pandas as pd
 
+df=pd.read_csv('/content/master.csv')
+
+st.header("สถิติการฆ่าตัวต่ายในปี 1987 - 2014")
+
+st.write(dt.head(10))
+
+st.subheader("จำนวนผู้เสียชีวิต")
+df['year'].hist(bins=[1987,1990,1995,2000,2005,2010,2014])
