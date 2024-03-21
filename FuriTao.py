a7c0b1fae1a0363de5ebdb295b24961782b8c089
CoffinWingS <156872580+CoffinWingS@users.noreply.github.com>
2024-03-21 19:58:08 +0700

    Update FuriTao.py

--- FuriTao.py
+++ FuriTao.py
@@ -50,6 +50,12 @@ st.subheader("โนมโน้มการฆ่าตัวตายในช
 
 
 if(st.button("แสดงโนมโน้มการฆ่าตัวตายในช่วงปี")):
+
+ # เลือกประเทศ
+ selected_country = st.selectbox("เลือกประเทศ", df['country'].unique())
+
+ # กรองข้อมูลตามประเทศที่เลือก
+ filtered_df = df[df['country'] == selected_country]
  # นับจำนวนการฆ่าตัวตายในแต่ละปี
  df_year = df.groupby('year').size()
 
@@ -110,37 +116,5 @@ ax.legend()
 
 st.pyplot(fig)
 #***************************************************************************************************
-st.header("สถิติการฆ่าตัวต่ายในปี 1987 - 2014")
 
-st.write(df.head(10))
-
-st.subheader("อัตราการฆ่าตัวตายในแต่ละประเทศ")
-
-# เลือกประเทศ
-selected_country = st.selectbox("เลือกประเทศ", df['country'].unique())
-
-# กรองข้อมูลตามประเทศที่เลือก
-filtered_df = df[df['country'] == selected_country]
-
-# นับจำนวนการฆ่าตัวตายในแต่ละประเทศ
-df_country = filtered_df.groupby('country').size()
-
-# เรียงลำดับประเทศตามจำนวนการฆ่าตัวตาย
-df_country = df_country.sort_values(ascending=False)
-
-# ปรับขนาดกราฟ
-plt.figure(figsize=(22, 50))
-
-# เพิ่มกริด
-plt.grid(axis='x')
-
-# เพิ่มเครื่องหมายบอกค่าบนแท่ง
-for i, v in enumerate(df_country):
-    plt.text(v, i, f'{v:,}', ha='right', va='center')
-
-# แสดงกราฟแท่งแนวนอน
-plt.barh(df_country.index, df_country, color='#ff5722')
-plt.xlabel('จำนวนการฆ่าตัวตาย')
-plt.ylabel('ประเทศ')
-plt.show()
 
